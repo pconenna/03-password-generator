918da77da94bdb43fdcbdb72b7405185747320b3
Peter Conenna <pconenna@depaul.edu>
2023-04-13 18:25:04 -0500

    made arrays with all the possible characters for a password

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //the button
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //this has to be a string
+  var passwordText = document.querySelector("#password"); //the output textarea
 
   passwordText.value = password;
 
 }
+// first thing to do would be to contain all the possible characters to make a password
+// so thats both cases of letter and special chars oh and numbers
+var letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var numbers = ['0', '1', '2', '3','4','5','6','7','8','9',]
+
+
+var specialChars = [" ", "!", '"', "#", "$", "%", "^", "&", "*", "(", ")", "+", 
+"-", ",", ".", "/", ":", ";", "'", "=",
+"<", ">", "?", "{", "}", "@", "[", "]", "\\", "`", "~", "|"] 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
