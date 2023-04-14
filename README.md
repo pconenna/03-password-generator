63d0a468d0b9dc471b75e94933c95906750085b8
Peter Conenna <pconenna@depaul.edu>
2023-04-14 15:11:24 -0500

    finished readme and cleaned up code

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## Password generator
 
 # Description
-This is going to be a password generator. 
+This is a password generator. The users presses a button and answers questions to determine the structure of the password. The generator will then return them a randomly generated password based on the user's parameters.
+# Installation
+N/A
+# Usage
+When the user opens the page they see with the empty text area and the generate password button. When they press the button a series of alert boxes appear. The first one will ask for a number of characters and will not allow the user to progress if they enter invalid input. If they enter valid input they will then see alert boxes asking them if they want to use lowercase letters, uppercase letter, numbers, and special characters. Every choice they agree to will be noted by the program. If the users says no to every possible choce the alerts will replay the whole cycle. The user must select at least one parameter. After the user makes their selections the program will generate a password and return it in the text area.
+# Credits 
+I used the mini project from the JS unit as a reference to make sure I was using alerts and prompts correctly.
+# License 
+Please refer to the license in the repo.
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //the button
 
-// first thing to do would be to contain all the possible characters to make a password
-// so thats both cases of letter and special chars oh and numbers
+//contain all possible characters
 var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -30,23 +29,30 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//doing this all in one function is kind of ugly
-function gatherRequirements(){
-  //make it impossible to mark no for every choice 
+
+function gatherRequirements(){ 
+//function to get user inputs
+  //reset these so rules change every time user presses button
+  numchars = 0;
+  hasLowerCase = false;
+  hasUpperCase = false;
+  hasNumbers = false;
+  hasSpecialChars = false;
+
   numchars = window.prompt("Choose an amount of characters between 8 and 128.");
   while(numchars < 8 || numchars > 128){
     numchars = window.prompt("Please try again the value must be between 8 and 128.");
   }
 
-  //this should make it impossible to generate a password after marking no for every choice
+  //this makes it impossible to generate a password after marking no for every choice
   while(!hasLowerCase && !hasUpperCase && !hasNumbers && !hasSpecialChars){
     hasLowerCase = window.confirm("Do you want to use lowercase letters?");
   
     hasUpperCase = window.confirm("Do you want to use uppercase letters?");
 
-    hasNumbers = window.confirm("do you want to use numbers?");
+    hasNumbers = window.confirm("Do you want to use numbers?");
 
-    hasSpecialChars = window.confirm("Do you want to use special characters")
+    hasSpecialChars = window.confirm("Do you want to use special characters?")
 
   }
   
@@ -54,6 +60,7 @@ function gatherRequirements(){
 } 
 
 function generatePassword(){
+  //generate password based on user inputs
   gatherRequirements();
   var pw = "";
   var charPool = [];
@@ -62,14 +69,14 @@ function generatePassword(){
   if(hasUpperCase){charPool = charPool.concat(upperCase)}
   if(hasNumbers){charPool = charPool.concat(numbers)}
   if(hasSpecialChars){charPool = charPool.concat(specialChars)}
-  console.log(charPool)
+  // console.log(charPool)
   for(var i = 0; i < numchars; i++){
     var randomIndex = Math.floor(Math.random()*charPool.length);
     var c = charPool[randomIndex]; 
     pw += c;
   }
-  console.log(pw);
-  console.log(pw.length);
+  // console.log(pw);
+  // console.log(pw.length);
   return pw;
 }
 
