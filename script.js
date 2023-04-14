ee219833424123796d08fa574a4b4b1f9b4fc39e
Peter Conenna <pconenna@depaul.edu>
2023-04-14 13:59:10 -0500

    made another array to separate upper and lowercase letters

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ function writePassword() {
 }
 // first thing to do would be to contain all the possible characters to make a password
 // so thats both cases of letter and special chars oh and numbers
-var letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var numbers = ['0', '1', '2', '3','4','5','6','7','8','9',]
 
@@ -21,6 +22,12 @@ var numbers = ['0', '1', '2', '3','4','5','6','7','8','9',]
 var specialChars = [" ", "!", '"', "#", "$", "%", "^", "&", "*", "(", ")", "+", 
 "-", ",", ".", "/", ":", ";", "'", "=",
 "<", ">", "?", "{", "}", "@", "[", "]", "\\", "`", "~", "|"] 
+
+//pw reqs:
+//8-128 chars
+//ask about char types (numbers, letter cases, and special characters)
+// use alerts
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
