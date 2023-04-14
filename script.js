8146ab9faccc34b98e254c8366364d8537e49aff
Peter Conenna <pconenna@depaul.edu>
2023-04-14 14:10:51 -0500

    built input for password length

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //the button
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  //this has to be a string
-  var passwordText = document.querySelector("#password"); //the output textarea
-
-  passwordText.value = password;
-
-}
 // first thing to do would be to contain all the possible characters to make a password
 // so thats both cases of letter and special chars oh and numbers
 var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -22,6 +13,31 @@ var numbers = ['0', '1', '2', '3','4','5','6','7','8','9',]
 var specialChars = [" ", "!", '"', "#", "$", "%", "^", "&", "*", "(", ")", "+", 
 "-", ",", ".", "/", ":", ";", "'", "=",
 "<", ">", "?", "{", "}", "@", "[", "]", "\\", "`", "~", "|"] 
+
+var numchars = 0;
+var hasLowerCase = false;
+var hasUpperCase = false;
+var hasNumbers = false;
+var hasSpecialChars = false;
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); //need to write this
+  //this has to be a string
+  var passwordText = document.querySelector("#password"); //the output textarea
+
+  passwordText.value = password;
+
+}
+
+function generatePassword(){
+  numchars = window.prompt("Choose an amount of characters between 8 and 128.")
+  while(numchars < 8 || numchars > 128){
+    numchars = window.prompt("Please try again the value must be between 8 and 128.")
+  }
+  hasLowerCase = window.confirm("Do you want to use lowercase letters?")
+}
+
 
 //pw reqs:
 //8-128 chars
